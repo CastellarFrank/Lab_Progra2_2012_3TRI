4eb13b0015df683cf3854e4303beefdfb9244795
Franklin <frank_nighmare@unitec.edu>
2012-08-10 09:43:17 -0600

    Subiendo el paquete de Generación de Objetos

---
+++ src/GenerarObjetos/Users.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GenerarObjetos;
+
+/**
+ *
+ * @author NIGHTMARE
+ */
+public class Users {
+    public String nombre;
+    public String direccion;
+    public char Sexo;
+    public String path;
+    public Users(String nombre, String dir,char sex,String path){
+        this.nombre=nombre;
+        this.direccion=dir;
+        this.Sexo=sex;
+        this.path=path;
+    }    
+}
